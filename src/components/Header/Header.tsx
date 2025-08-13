8eb0179a70dc617735962f0abbf5ea260da7a0bb
Kim <kevin2695@naver.com>
2025-08-14 00:44:56 +0900

    feat : header 하단 border color 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import MainLogo from "@/images/main_logo.svg";
 
 const Header = () => {
   return (
-    <div className="w-full border-b-[0.5px] border-white px-[24px] py-[16px] flex items-center">
+    <div className="w-full border-b border-gray-600 px-[24px] py-[16px] flex items-center">
       <MainLogo />
       <p className="ml-[10px] text-xl font-bold">HackerRank AI Helper</p>
     </div>
