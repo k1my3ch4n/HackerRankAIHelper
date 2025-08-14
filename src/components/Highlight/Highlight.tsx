7f20db2ed4ac40b9575a04a35708ea4b9e7e1d2b
Kim <kevin2695@naver.com>
2025-08-14 19:35:24 +0900

    feat : Highlight 컴포넌트 수정

--- src/components/Highlight/Highlight.tsx
+++ src/components/Highlight/Highlight.tsx
@@ -1,3 +1,5 @@
+// todo : 메인 색으로 설정, 추후 흰색 / 메인 색 나누어서 설정
+
 const Highlight = ({
   text = "",
   className = "",
@@ -5,7 +7,7 @@ const Highlight = ({
   text?: string;
   className?: string;
 }) => {
-  return <span className={`${className} text-white`}>{text}</span>;
+  return <span className={`${className} text-[#01E92C]`}>{text}</span>;
 };
 
 export default Highlight;
