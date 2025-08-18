ac5a129ef9c66bf32c09494bcffe33d931d313e0
Kim <kevin2695@naver.com>
2025-08-18 19:14:40 +0900

    feat : NavigateButton 컴포넌트 생성

---
+++ src/components/NavigateButton/NavigateButton.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const NavigateButton = ({
+  url = "/",
+  theme = "white",
+  className = "",
+  text = "",
+}: {
+  url?: string;
+  theme?: "main" | "white";
+  className?: string;
+  text?: string;
+}) => {
+  const router = useRouter();
+
+  const themeClassName = {
+    white: "border border-white bg-white",
+    main: "border border-[#01E92C] bg-[#01E92C]",
+  };
+
+  const buttonClassName =
+    "px-[16px] py-[12px] rounded-xl text-black font-semibold cursor-pointer";
+
+  const handleClick = () => {
+    router.push(url);
+  };
+  return (
+    <button
+      className={`${buttonClassName} ${themeClassName[theme]} ${className}`}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default NavigateButton;
