07a2740d99795cb26c295d1a224ea7a978b3a23c
Kim <kevin2695@naver.com>
2025-08-29 16:06:08 +0900

    feat : prompt , page 임시 수정

--- src/app/_components/Prompt/Prompt.tsx
+++ src/app/_components/Prompt/Prompt.tsx
@@ -1,6 +1,13 @@
+// todo : handleClick 타입 수정
+// todo : type 중복
+
+"use client";
+
 import ReactMarkdown from "react-markdown";
 import Highlight from "@/components/Highlight";
 import Button from "@/components/Button";
+import useToggle from "@/hooks/useToggle";
+import QuestionForm from "../QuestionForm";
 
 type TypeKey = "summary" | "hint" | "answer";
 
@@ -15,40 +22,50 @@ const Prompt = ({
   summary,
   problemTitle,
   onClick,
+  isLastElement,
+  onToggle,
 }: {
   type: TypeKey;
   problemTitle: string;
   summary: string;
   onClick: (type: "summary" | "hint" | "answer") => void;
+  isLastElement: boolean;
+  onToggle: () => void;
 }) => {
   return (
-    <div className="w-1/2 p-[20px] mt-[10px] border border-gray-800 rounded-xl bg-gray-800">
-      <p className="pb-[10px]">
-        문제 <Highlight text={PROMPT_TYPE[type]} /> : {problemTitle}
-      </p>
+    <>
+      <div className="w-1/2 p-[20px] mt-[10px] border border-gray-800 rounded-xl bg-gray-800">
+        <p className="pb-[10px]">
+          문제 <Highlight text={PROMPT_TYPE[type]} /> : {problemTitle}
+        </p>
 
-      <ReactMarkdown children={summary} />
+        <ReactMarkdown children={summary} />
 
-      <div className="w-full flex pt-[10px] ">
-        {Object.entries(PROMPT_TYPE).map(([key, value]) => {
-          return (
-            <Button
-              key={key}
-              className="grow mr-[10px]"
-              theme="white"
-              onClick={() => onClick(key as TypeKey)}
-            >
-              {key === type ? "다른 " : ""}
-              {value}
-            </Button>
-          );
-        })}
+        {isLastElement && (
+          <>
+            <div className="w-full flex pt-[10px] ">
+              {Object.entries(PROMPT_TYPE).map(([key, value]) => {
+                return (
+                  <Button
+                    key={key}
+                    className="grow mr-[10px]"
+                    theme="white"
+                    onClick={() => onClick(key as TypeKey)}
+                  >
+                    {key === type ? "다른 " : ""}
+                    {value}
+                  </Button>
+                );
+              })}
 
-        <Button className="grow" theme="main">
-          다른 문제 풀기
-        </Button>
+              <Button className="grow" theme="main" onClick={onToggle}>
+                다른 문제 풀기
+              </Button>
+            </div>
+          </>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
--- src/app/helper/page.tsx
+++ src/app/helper/page.tsx
@@ -1,12 +1,15 @@
-// todo : handleClick 타입 수정
+// todo : prompt 임시 제거
+// todo : problem << 전역 상태로
 
 "use client";
 
 import useGeminiApi from "@/api/useGeminiApi";
 import Button from "@/components/Button";
 import Highlight from "@/components/Highlight";
-import { problemInputFilter } from "@/utils/regexUtils";
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+import QuestionForm from "../_components/QuestionForm";
+import useToggle from "@/hooks/useToggle";
 
 import ReactMarkdown from "react-markdown";
 
@@ -24,34 +27,17 @@ const PROMPT_TYPE: Record<TypeKey, string> = {
 };
 
 const helperPage = () => {
+  const questionNameRef = useRef("");
+
   const [problem, setProblem] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string>("");
   const [prompt, setPrompt] = useState<PromptDataType[]>([]);
 
   const { fetchGeminiData } = useGeminiApi();
 
-  const handleChangeProblem = (value: string) => {
-    if (errorMessage) {
-      setErrorMessage("");
-    }
-
-    setProblem(value);
-  };
-
-  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const { isValid, problemName } = problemInputFilter({ problem });
-
-    if (isValid) {
-      await fetchGeminiData({ problemTitle: problemName, setPrompt });
-    } else {
-      setErrorMessage("잘못된 URL 또는 잘못된 문제 이름입니다.");
-    }
-  };
+  const { isToggle, handleToggle } = useToggle(false);
 
   const handleFetchClick = async (type: "summary" | "hint" | "answer") => {
-    const { problemName } = problemInputFilter({ problem });
+    const problemName = questionNameRef.current;
 
     await fetchGeminiData({ problemTitle: problemName, setPrompt, type });
   };
@@ -60,38 +46,52 @@ const helperPage = () => {
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-65px)]">
       {prompt.length > 0 ? (
         prompt.map(({ type, summary, problemTitle }, index) => {
-          console.log(summary);
           return (
-            <div
-              className="w-1/2 p-[20px] mt-[10px] border border-gray-800 rounded-xl bg-gray-800"
-              key={index}
-            >
-              <p className="pb-[10px]">
-                문제 <Highlight text={PROMPT_TYPE[type]} /> : {problemTitle}
-              </p>
+            <>
+              <div className="w-1/2 p-[20px] mt-[10px] border border-gray-800 rounded-xl bg-gray-800">
+                <p className="pb-[10px]">
+                  문제 <Highlight text={PROMPT_TYPE[type]} /> : {problemTitle}
+                </p>
 
-              <ReactMarkdown children={summary} />
+                <ReactMarkdown children={summary} />
 
-              <div className="w-full flex pt-[10px] ">
-                {Object.entries(PROMPT_TYPE).map(([key, value]) => {
-                  return (
-                    <Button
-                      key={key}
-                      className="grow mr-[10px]"
-                      theme="white"
-                      onClick={() => handleFetchClick(key as TypeKey)}
-                    >
-                      {key === type ? "다른 " : ""}
-                      {value}
-                    </Button>
-                  );
-                })}
+                {index === prompt.length - 1 && (
+                  <>
+                    <div className="w-full flex pt-[10px] ">
+                      {Object.entries(PROMPT_TYPE).map(([key, value]) => {
+                        return (
+                          <Button
+                            key={key}
+                            className="grow mr-[10px]"
+                            theme="white"
+                            onClick={() => handleFetchClick(key as TypeKey)}
+                          >
+                            {key === type ? "다른 " : ""}
+                            {value}
+                          </Button>
+                        );
+                      })}
 
-                <Button className="grow" theme="main">
-                  다른 문제 풀기
-                </Button>
+                      <Button
+                        className="grow"
+                        theme="main"
+                        onClick={handleToggle}
+                      >
+                        다른 문제 풀기
+                      </Button>
+                    </div>
+                  </>
+                )}
               </div>
-            </div>
+              {index === prompt.length - 1 && isToggle && (
+                <QuestionForm
+                  question={problem}
+                  setQuestion={setProblem}
+                  setPrompt={setPrompt}
+                  questionRef={questionNameRef}
+                />
+              )}
+            </>
           );
         })
       ) : (
@@ -99,29 +99,12 @@ const helperPage = () => {
           <p className="font-medium text-4xl my-[20px]">
             어떤 <Highlight text="문제" />를 도와드릴까요 ?
           </p>
-          <form
-            className={`w-1/2 flex mx-[20px] px-[16px] py-[12px] rounded-xl bg-gray-800 relative border border-gray-800 ${
-              errorMessage ? "border border-red-400" : ""
-            }`}
-            onSubmit={(e) => handleFormSubmit(e)}
-          >
-            <div className="flex w-full">
-              <input
-                className="w-full text-white focus:outline-hidden "
-                value={problem}
-                placeholder="문제 URL 또는 문제 이름을 입력하세요."
-                onChange={(e) => handleChangeProblem(e.target.value)}
-              />
-              <Button className="ml-[10px]" theme="main">
-                찾기
-              </Button>
-            </div>
-            {errorMessage && (
-              <p className="absolute bottom-[-24px] text-red-400">
-                {errorMessage}
-              </p>
-            )}
-          </form>
+          <QuestionForm
+            question={problem}
+            setQuestion={setProblem}
+            setPrompt={setPrompt}
+            questionRef={questionNameRef}
+          />
         </>
       )}
     </div>
