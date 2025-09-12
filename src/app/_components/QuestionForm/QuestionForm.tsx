7c962ceeaca5f1eec607dff56f4f814db54f914a
Kim <kevin2695@naver.com>
2025-09-12 17:25:44 +0900

    feat : helper 페이지 반응형 추가

--- src/app/_components/QuestionForm/QuestionForm.tsx
+++ src/app/_components/QuestionForm/QuestionForm.tsx
@@ -41,7 +41,7 @@ const QuestionForm = ({ handleOff }: { handleOff: () => void }) => {
 
   return (
     <form
-      className={`w-1/2 flex m-[20px] px-[16px] py-[12px] rounded-xl relative border bg-gray-800 ${
+      className={`w-3/4 md:w-1/2 flex m-[20px] px-[16px] py-[12px] rounded-xl relative border bg-gray-800 ${
         errorMessage ? "border-red-400" : "border-gray-800"
       }`}
       onSubmit={(e) => handleFormSubmit(e)}
