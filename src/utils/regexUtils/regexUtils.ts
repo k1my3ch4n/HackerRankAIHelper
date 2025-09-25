76ed810cbed43e896d8f5793fe31a3a11b49aeab
Kim <kevin2695@naver.com>
2025-09-25 16:31:24 +0900

    feat : regex input or url -> url 만 수용되도록 변경

--- src/utils/regexUtils/regexUtils.ts
+++ src/utils/regexUtils/regexUtils.ts
@@ -1,23 +1,12 @@
-const INPUT_REGEX =
-  /^((https:\/\/www\.hackerrank\.com\/challenges\/.+)|([a-zA-Z0-9 ]+))$/;
-
-const START_URL = "https://www.hackerrank.com/challenges/";
+const INPUT_REGEX = /^((https:\/\/www\.hackerrank\.com\/challenges\/.+))$/;
 
 export const questionInputFilter = ({
   questionInput,
 }: {
   questionInput: string;
 }) => {
-  let questionName = questionInput;
-
-  if (questionInput.startsWith(START_URL)) {
-    const splitUrl = questionInput.split("/");
-
-    questionName = splitUrl[4].replace(/-/g, " ");
-  }
-
   return {
     isValid: INPUT_REGEX.test(questionInput),
-    questionName,
+    url: questionInput,
   };
 };
