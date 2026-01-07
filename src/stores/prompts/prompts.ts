e3e8654c53fc49a892c806f513aca15b06a686f3
Kim <kevin2695@naver.com>
2026-01-07 20:16:14 +0900

    feat : 상태 초기화 메서드 추가

--- src/stores/prompts/prompts.ts
+++ src/stores/prompts/prompts.ts
@@ -4,12 +4,14 @@ import type { PromptData } from "@/types";
 interface PromptsState {
   prompts: PromptData[];
   setPrompts: (newPrompt: PromptData) => void;
+  clearPrompts: () => void;
 }
 
 const usePrompts = create<PromptsState>((set) => ({
   prompts: [],
   setPrompts: (newPrompt) =>
     set((state) => ({ prompts: [...state.prompts, newPrompt] })),
+  clearPrompts: () => set({ prompts: [] }),
 }));
 
 export default usePrompts;
--- src/stores/questionURL/questionURL.ts
+++ src/stores/questionURL/questionURL.ts
@@ -3,11 +3,13 @@ import { create } from "zustand";
 interface QuestionURLState {
   questionURL: string;
   setQuestionURL: (newValue: string) => void;
+  clearQuestionURL: () => void;
 }
 
 const useQuestionURL = create<QuestionURLState>((set) => ({
   questionURL: "",
   setQuestionURL: (newValue) => set({ questionURL: newValue }),
+  clearQuestionURL: () => set({ questionURL: "" }),
 }));
 
 export default useQuestionURL;
